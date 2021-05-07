42d1f60a0d6949bcfedc522de31b6ea0616d5803
Eduardo dos Santos <eduardosantosj2@gmail.com>
2021-05-07 01:43:32 -0300

    resolve deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { resolve } = require('path')
+
+const app = express()
+
+app.use('/', express.static(
+  resolve(
+    __dirname,
+    './build'
+  )
+))
+
+app.listen(process.env.PORT || 8000, (error) => {
+  if(error) {return console.log(error)}
+
+  return console.log('tudo certo')
+})
